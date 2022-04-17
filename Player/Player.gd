b0729801a94dafc0a0c8f264cb6e91e9b6d74623
Henrique de Abreu <hpereira986@gmail.com>
2022-04-17 20:12:29 -0300

    Movimentação + animações

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,9 +1,11 @@
 extends KinematicBody
 
-const GRAVITY = 100
 const SPEED = 10
+const GRAVITY = 100
 
 var velocity = Vector3.ZERO
+
+onready var sprite = $AnimatedSprite3D
 
 func _physics_process(delta):
 	movement(delta)
@@ -13,15 +15,27 @@ func movement(delta):
 	
 	if Input.is_action_pressed("ui_right"):
 		position.x += 1
-	if Input.is_action_pressed("ui_left"):
+		
+		sprite.flip_h = false
+		sprite.play('Move')
+	elif Input.is_action_pressed("ui_left"):
 		position.x -= 1
-	if Input.is_action_pressed("ui_down"):
+		
+		sprite.flip_h = true
+		sprite.play('Move')
+	elif Input.is_action_pressed("ui_down"):
 		position.z += 1
-	if Input.is_action_pressed("ui_up"):
+		sprite.play('Move')
+	elif Input.is_action_pressed("ui_up"):
 		position.z -= 1
+		sprite.play('Move')
+	else:
+		sprite.play('Idle')
+	
 	
 	velocity.x = position.x * SPEED
 	velocity.z = position.z * SPEED
+	
 	velocity.y -= GRAVITY * delta
 	
 	velocity = move_and_slide(velocity, Vector3.UP)
